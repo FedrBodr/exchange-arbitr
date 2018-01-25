deeb79812a3b62a1ff96228d657f907349fa2588
Dmitry Fedorenko <fedrbodr@gmail.com>
2018-01-25 17:36:31 +0300

    Initial commit

---
+++ src/main/java/ru/fedrbodr/exchangearbitr/ExchangeArbitrApplication.java
@@ -0,0 +1,12 @@
+package ru.fedrbodr.exchangearbitr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeArbitrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeArbitrApplication.class, args);
+	}
+}
